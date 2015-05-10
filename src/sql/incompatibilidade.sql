50cb005ad32a0a9844f494a73b13d8af95a23ce9
sacramento <sacramento@Timoteo>
2015-05-10 14:55:00 -0300

    adiçao da funçao incompatibilidade.sql .. tem que ser inserida no banco de dados

---
+++ src/sql/incompatibilidade.sql
@@ -0,0 +1,25 @@
+﻿CREATE OR REPLACE FUNCTION incompatibilidade(p_numonu integer)
+	RETURNS TABLE (
+		numonu integer
+		) AS
+		$$
+		BEGIN
+			RETURN QUERY
+			SELECT 
+				c.numonu
+			FROM 
+				produto p,
+				tipo_comp t,
+				compatibilidade c
+			WHERE
+				p.num_onu = p_numonu
+				AND
+				p.classe = t.id_classe
+				AND
+				t.id_tipo_comp = c.id_tipo_comp;
+
+		END;
+		$$
+			LANGUAGE plpgsql;
+	ALTER FUNCTION incompatibilidade(integer)
+	OWNER TO slcpp;
