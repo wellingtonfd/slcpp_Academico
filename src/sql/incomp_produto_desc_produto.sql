005ba7918f99661c75da7b58684e9ef3edeba218
sacramento <sacramento@Galatas>
2015-07-03 17:37:59 -0300

    funcao incompatibilidade com desc_produto

---
+++ src/sql/incomp_produto_desc_produto.sql
@@ -0,0 +1,12 @@
+﻿CREATE OR REPLACE FUNCTION incomp_produto_desc_produto(IN p_numonu integer)
+  RETURNS SETOF integer AS
+$BODY$
+		BEGIN
+			RETURN QUERY
+			SELECT p2.numonu , p1.desc_produto from incompatibilidade(p_numonu) as p2 inner join produto as p1 on p1.num_onu = p2.numonu;
+
+		END;
+		$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
---
+++ src/sql/incompatibilidade_desc_produto.sql
@@ -0,0 +1,1 @@
+SELECT p2.numonu , p1.desc_produto from incompatibilidade(3221) as p2 inner join produto as p1 on p1.num_onu = p2.numonu;
