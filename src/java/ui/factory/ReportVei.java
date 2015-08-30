2078266daf0612d922047e0b1827079c50fd7888
wellington <wellington@TI_EFETIVO>
2015-08-30 20:03:26 -0300

    Criação tela saída Produto Signed-off-by:wellington <wellington@TI_EFETIVO>

--- src/java/ui/factory/ReportVei.java
+++ src/java/ui/factory/ReportVei.java
@@ -22,7 +22,7 @@ public class ReportVei {
         return nomeReport;
     }
 
-    String jr = FacesContext.getCurrentInstance().getExternalContext().getRealPath("WEB-INF/reports/Relatorio_Veicuto/RelatorioVeiculo.jasper");
+    String jr = FacesContext.getCurrentInstance().getExternalContext().getRealPath("WEB-INF/reports/Relatorio_Veiculo/RelatorioVeiculo.jasper");
 
     public StreamedContent getArquivoRetorno() {
         FacesContext context = FacesContext.getCurrentInstance();
